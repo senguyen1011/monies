cea2633aca1ebae5ba4c2bf5153523d197e57cef
senguyen1011 <seantanhnguyen@gmail.com>
2020-08-03 18:25:29 -0400

    Updates

---
+++ precache-manifest.202a77a3de3118c3f20193144ede7667.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa325cfdbd2c192152b26d59b5db3fe3",
+    "url": "/monies/index.html"
+  },
+  {
+    "revision": "00e1cca2c92b120040e0",
+    "url": "/monies/static/css/main.b88f933d.chunk.css"
+  },
+  {
+    "revision": "40059d5d75b19978c312",
+    "url": "/monies/static/js/2.9fa7654e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monies/static/js/2.9fa7654e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00e1cca2c92b120040e0",
+    "url": "/monies/static/js/main.eea0711c.chunk.js"
+  },
+  {
+    "revision": "0d1cba02cf754442c848",
+    "url": "/monies/static/js/runtime-main.5636dd0e.js"
+  }
+]);
